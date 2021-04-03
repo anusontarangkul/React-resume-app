dd40e72ea318183cdab495715e83b8907e6efb54
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-04-02 23:03:13 -0700

    map through jobs

--- src/App.js
+++ src/App.js
@@ -33,6 +33,25 @@ function App() {
         <h2>Experience</h2>
         <div className="underline"></div>
       </div>
+      <div className="jobs-center">
+        {/* {btn container} */}
+        {/* {job info} */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+
+      </div>
+
     </section>
   );
 }
